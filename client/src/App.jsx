df2b1503655e94e6da80d93846d8c235c02c998b
Aman Kumar <151604983+AmanKumar9958@users.noreply.github.com>
2025-10-08 05:13:48 +0530

    Added lazy & suspense while loading the pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,28 @@
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
-import Jobs from './pages/Jobs'
-import ApplyJob from './pages/ApplyJob'
-import Applications from './pages/Applications'
-import AppLayout from './components/AppLayout'
-import LandingPage from './pages/LandingPage'
-import RecruiterLogin from './components/RecruiterLogin'
 import { useContext } from 'react'
 import { AppContext } from './context/AppContext'
-import RecruiterDashboard from './pages/RecruiterDashboard'
-import AddJob from './pages/AddJob'
-import ManageJobs from './pages/ManageJobs'
-import ViewApplications from './pages/ViewApplications'
-import 'quill/dist/quill.snow.css'
+import { lazy, Suspense } from 'react';
+
+// Dynamically import components, only when user visits the route
+const Jobs = lazy(() => import('./pages/Jobs'));
+const ApplyJob = lazy(() => import('./pages/ApplyJob'));
+const Applications = lazy(() => import('./pages/Applications'));
+const AppLayout = lazy(() => import('./components/AppLayout'));
+const LandingPage = lazy(() => import('./pages/LandingPage'));
+const RecruiterLogin = lazy(() => import('./components/RecruiterLogin'));
+const RecruiterDashboard = lazy(() => import('./pages/RecruiterDashboard'));
+const AddJob = lazy(() => import('./pages/AddJob'));
+const ManageJobs = lazy(() => import('./pages/ManageJobs'));
+const ViewApplications = lazy(() => import('./pages/ViewApplications'));
+
+import 'quill/dist/quill.snow.css'  // for quill editor
+
+// Simple loading fallback component
+const LoadingSpinner = () => (
+  <div className="flex items-center justify-center min-h-screen">
+      <div className="w-16 h-16 border-4 border-blue-500 border-dashed rounded-full animate-spin" />
+  </div>
+);
 
 const App = () => {
 
@@ -21,7 +32,8 @@ const App = () => {
     <div>
       <RecruiterLogin open={showRecruiterLogin} onClose={() => setShowRecruiterLogin(false)} />
       <BrowserRouter>
-        <Routes>
+        <Suspense fallback={LoadingSpinner() }>
+          <Routes>
             <Route element={<AppLayout />}>
                 <Route path="/" element={<LandingPage />} />
                 <Route path="/all-jobs" element={<Jobs />} />
@@ -34,7 +46,8 @@ const App = () => {
                   <Route path="view-applications" element={<ViewApplications />} />
                 </Route>
             </Route>
-        </Routes>
+          </Routes>
+        </Suspense>
       </BrowserRouter>
     </div>
   )
